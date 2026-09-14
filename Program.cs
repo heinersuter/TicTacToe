6fd1611775677fde26304b3f37cf504efc9c415a
Heiner Suter <heiner.suter@zuehlke.com>
2026-09-14 17:40:46 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using TicTacToe;
+
+var board = new Board();
+
+board.A1 = Wert.X;
+board.B2 = Wert.O;
+board.C3 = Wert.X;
+board.C2 = Wert.O;
+board.A2 = Wert.O;
+board.A2 = Wert.X;
---
+++ Wert.cs
@@ -0,0 +1,8 @@
+namespace TicTacToe;
+
+public enum Wert
+{
+    X,
+    O,
+    Leer,
+}
